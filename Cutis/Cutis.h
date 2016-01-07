e6d8b13afd21e59d5f04aba60890ef289718077c
Tamas Lustyik <elveestei@gmail.com>
2016-01-07 22:49:17 +0100

    Initial Commit

---
+++ Cutis/Cutis.h
@@ -0,0 +1,19 @@
+//
+//  Cutis.h
+//  Cutis
+//
+//  Created by Tamas Lustyik on 2016. 01. 07..
+//  Copyright © 2016. Tamas Lustyik. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Cutis.
+FOUNDATION_EXPORT double CutisVersionNumber;
+
+//! Project version string for Cutis.
+FOUNDATION_EXPORT const unsigned char CutisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cutis/PublicHeader.h>
+
+
