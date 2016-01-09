dd80e450f6330d0230f984fa1c27ed99aae518c4
Tamas Lustyik <elveestei@gmail.com>
2016-01-10 00:08:09 +0100

    More headerdoc

--- Cutis/OSType.swift
+++ Cutis/OSType.swift
@@ -10,6 +10,12 @@ import Foundation
 
 
 extension String {
+    /// A helper function to canonically encode an OSType as a CFString
+    /// suitable for use as a tag argument.
+    ///
+    /// - parameter osType: the OSType value to encode
+    ///
+    /// - returns: a string instance
     public init(osType: OSType) {
         let str = UTCreateStringForOSType(osType).takeRetainedValue()
         self.init(str as NSString)
@@ -17,6 +23,13 @@ extension String {
 }
 
 extension OSType {
+    /// A helper function to canonically decode a string-encoded OSType
+    /// back to the original OSType value.
+    ///
+    /// - parameter string: the string to decode
+    ///
+    /// - returns: the OSType value encoded in the string, or 0 if the string is not
+    /// a valid encoding of an OSType
     public init(string: String) {
         self.init(UTGetOSTypeFromString(string))
     }
